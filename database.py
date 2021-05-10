71f367b35d4b688a586350272ec2528d4b670cc9
samfallowfield <73955537+samfallowfield@users.noreply.github.com>
2021-05-10 18:44:58 +0100

    Create database.py

---
+++ database.py
@@ -0,0 +1,51 @@
+from .database_connection import DatabaseConnection
+
+
+def create_book_table():
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)')
+
+
+
+
+def get_all_books():
+
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('SELECT * FROM books')
+        books = [{'name': row[0], 'author': row[1], 'read':[2] } for row in cursor.fetchall()]
+
+    return books
+
+
+
+def add_book(name, author):
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+        try:
+            cursor.execute('INSERT INTO books VALUES(?, ?, 0)', (name, author))
+
+        except:
+            print(
+                'This book has already been added to your list, please try again:')
+
+
+
+
+
+def mark_book_as_read(name):
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('UPDATE books SET read=1 WHERE name=?', (name,))
+
+
+
+def delete_book(name):
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('DELETE FROM books WHERE name=?',(name,))
